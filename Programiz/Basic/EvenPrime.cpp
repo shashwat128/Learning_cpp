b35f74195e6155b2600aeff27589a4051429e259
Shashwat Singh <shashwat2048@gmail.com>
2023-06-24 23:03:54 +0530

    even or odd

---
+++ Programiz/Basic/EvenPrime.cpp
@@ -0,0 +1,21 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num;
+
+    cout << "Enter an Integer: ";
+    cin >> num;
+
+    if (num % 2 == 0)
+    {
+        cout << num << " is even.";
+    }
+    else
+    {
+        cout << num << " is odd.";
+    }
+
+    return 0;
+}
