937036321d08db6c7b3dc67d4d1cd72487b82fa9
Shashwat Singh <shashwat2048@gmail.com>
2023-06-28 21:18:18 +0530

    factors of a number

---
+++ Programiz/Basic4/Factors.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int num, i;
+
+    cout << "Enter a positve integer: ";
+    cin >> num;
+
+    for (i = 1; i <= num; ++i)
+    {
+        if (num % i == 0)
+        {
+            cout << i << " ";
+        }
+    }
+
+    return 0;
+}
