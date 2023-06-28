35ba67789510bb150c5b3d29fe7dbae698747eda
Shashwat Singh <shashwat2048@gmail.com>
2023-06-28 13:48:09 +0530

    Prime number or not

---
+++ Programiz/Basic3/PrimeNum2.cpp
@@ -0,0 +1,33 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int main()
+{
+    int i, n;
+    bool is_prime = true;
+
+    cout << "Enter a positive integer: ";
+    cin >> n;
+
+    if (n == 0 || n == 1)
+    {
+        is_prime = false;
+    }
+
+    for (i = 2; i <= sqrt(n); ++i)
+    {
+        if (n % i == 0)
+        {
+            is_prime = false;
+            break;
+        }
+    }
+
+    if (is_prime)
+        cout << n << " is a Prime number.";
+    else
+        cout << n << " is not a Prime number.";
+
+    return 0;
+}
