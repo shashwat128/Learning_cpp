c1257f5fe73c7d9367b4f283cd02adbadb9e6fbf
Shashwat Singh <shashwat2048@gmail.com>
2023-07-01 11:08:49 +0530

    octal to decimal

---
+++ Programiz/Basic5Functions/ConvertNumerSystem/OctalToDecimal.cpp
@@ -0,0 +1,31 @@
+#include <iostream>
+#include <cmath>
+using namespace std;
+
+int octalToDecimal(int octalNumber);
+
+int main()
+{
+    int octalNumber;
+
+    cout << "Enter an octal Number: ";
+    cin >> octalNumber;
+
+    cout << octalNumber << " in octal = " << octalToDecimal(octalNumber) << " in decimal";
+
+    return 0;
+}
+
+int octalToDecimal(int octalNumber)
+{
+    int decimalNumber = 0, i = 0, rem;
+
+    while (octalNumber != 0)
+    {
+        rem = octalNumber % 10;
+        octalNumber /= 10;
+        decimalNumber = decimalNumber + rem * pow(8, i);
+        ++i;
+    }
+    return decimalNumber;
+}
