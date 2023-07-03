f8fa76e0e444b3d7a3bab7e393bd34f42652f61e
Shashwat Singh <shashwat2048@gmail.com>
2023-07-03 07:07:09 +0530

    Transpose of a matrix

---
+++ Programiz/BasicArrays/TransposeMatrix.cpp
@@ -0,0 +1,63 @@
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    int a[10][10], transpose[10][10], row, column, i, j;
+
+    cout << "Enter rows and columns of matrix: ";
+    cin >> row >> column;
+
+    cout << "\nEnter elements of matrix: " << endl;
+
+    // storing elements
+    for (i = 0; i < row; ++i)
+    {
+        for (j = 0; j < column; ++j)
+        {
+            cout << "Enter element a" << i + 1 << j + 1 << " : ";
+            cin >> a[i][j];
+        }
+    }
+
+    // printing the 'a' matrix
+    cout << "\nEntered Matrix: " << endl;
+    for (i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            cout << " " << a[i][j];
+            if (j == column - 1)
+            {
+                cout << endl
+                     << endl;
+            }
+        }
+    }
+
+    // transpose
+    for (i = 0; i < row; ++i)
+    {
+        for (int j = 0; j < column; ++j)
+        {
+            transpose[j][i] = a[i][j];
+        }
+    }
+
+    // printing transpose
+    cout << "\nTranspose of Matrix: " << endl;
+    for (i = 0; i < column; ++i)
+    {
+        for (int j = 0; j < row; ++j)
+        {
+            cout << " " << transpose[i][j];
+            if (j == row - 1)
+            {
+                cout << endl
+                     << endl;
+            }
+        }
+    }
+
+    return 0;
+}
