ac27be3bdd94e87272c3c5a64b30106a375c3aa2
Shashwat Singh <shashwat2048@gmail.com>
2023-07-05 08:52:26 +0530

    sort elements in Lexicographical order

---
+++ Programiz/Basic8/SortElements.cpp
@@ -0,0 +1,38 @@
+// Sort words in a dictonary
+#include <iostream>
+using namespace std;
+
+int main()
+{
+    string str[10], temp;
+
+    cout << "Enter 10 words: " << endl;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        getline(cin, str[i]);
+        str[i].erase(0, str[i].find_first_not_of(' '));
+        str[i].erase(str[i].find_last_not_of(' ') + 1);
+    }
+
+    for (int i = 0; i < 9; ++i)
+    {
+        for (int j = 0; j < 9 - i; ++j)
+        {
+            if (str[j] > str[j + 1])
+            {
+                temp = str[j];
+                str[j] = str[j + 1];
+                str[j + 1] = temp;
+            }
+        }
+    }
+
+    cout << "In lexicographical order: " << endl;
+
+    for (int i = 0; i < 10; ++i)
+    {
+        cout << str[i] << endl;
+    }
+    return 0;
+}
